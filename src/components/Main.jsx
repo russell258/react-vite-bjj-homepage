a104ca2e5b2d127c13dbd0506af48cc97ec5c4a5
Russell258 <duckie258@gmail.com>
2024-02-24 00:58:45 +0800

    background image and css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,13 @@ import React from "react"
 
 export default function Main(){
     return(
-        <h1>Main here</h1>
-    )
+        <div>
+            <h1 className="main--title">Fun Facts About us</h1>
+            <ul className="main--facts">
+                <li>Started in Dec 2020 and Apr 2021 respectively</li>
+                <li>70kg and 82kg weight category</li>
+                <li>First move we learnt was electric chair</li>
+            </ul>
+        </div>
+        )
 } 
